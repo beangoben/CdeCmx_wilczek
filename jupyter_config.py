fb34309ba88053fca694d8aac519cce324d9805b
Benjamin Sanchez <beangoben@gmail.com>
2016-07-01 14:02:22 -0400

    initial stuff

---
+++ jupyter_config.py
@@ -0,0 +1,46 @@
+# Configuration file for Jupyter Hub
+
+c = get_config()
+
+# spawn with Docker
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.JupyterHub.confirm_no_ssl = True
+c.LocalAuthenticator.create_system_users = True
+c.DockerSpawner.container_image ='myimage'
+# The docker instances need access to the Hub, so the default loopback port doesn't work:
+from jupyter_client.localinterfaces import public_ips
+c.JupyterHub.hub_ip = public_ips()[0]
+
+# OAuth with GitHub
+c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
+
+c.Authenticator.whitelist = whitelist = set()
+c.Authenticator.admin_users = admin = set()
+
+import os
+
+join = os.path.join
+here = os.path.dirname(__file__)
+with open(join(here, 'userlist')) as f:
+    for line in f:
+        if not line:
+            continue
+        parts = line.split()
+        name = parts[0]
+        whitelist.add(name)
+        if len(parts) > 1 and parts[1] == 'admin':
+            admin.add(name)
+# empty whitelist so we can make user dynamically
+c.Authenticator.whitelist = whitelist = set()
+
+c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+
+# ssl config
+ssl = join(here, 'ssl')
+keyfile = join(ssl, 'ssl.key')
+certfile = join(ssl, 'ssl.cert')
+if os.path.exists(keyfile):
+    c.JupyterHub.ssl_key = keyfile
+if os.path.exists(certfile):
+    c.JupyterHub.ssl_cert = certfile
+
